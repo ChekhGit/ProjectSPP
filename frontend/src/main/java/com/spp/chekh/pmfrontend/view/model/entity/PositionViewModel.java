7fe6078bd4632e71c09431615c800ed00ac9a5e5
ChekhDmitry <dimacheh97@mail.ru>
2018-02-12 12:47:59 +0300

    Small code fix.

--- frontend/src/main/java/com/spp/chekh/pmfrontend/view/model/entity/PositionViewModel.java
+++ frontend/src/main/java/com/spp/chekh/pmfrontend/view/model/entity/PositionViewModel.java
@@ -13,6 +13,14 @@ public class PositionViewModel {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "PositionViewModel{" +
