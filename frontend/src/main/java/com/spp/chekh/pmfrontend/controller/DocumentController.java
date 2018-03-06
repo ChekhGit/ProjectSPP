e2de6d7f544df6ce9471ec3db8f77229f6c3ebab
ChekhDmitry <dimacheh97@mail.ru>
2018-03-06 20:31:59 +0300

    Document controller has been edited.

--- frontend/src/main/java/com/spp/chekh/pmfrontend/controller/DocumentController.java
+++ frontend/src/main/java/com/spp/chekh/pmfrontend/controller/DocumentController.java
@@ -1,45 +1,26 @@
 package com.spp.chekh.pmfrontend.controller;
 
-import com.spp.chekh.pmbackend.document.view.*;
-import com.spp.chekh.pmbackend.entity.CoachEntity;
-import com.spp.chekh.pmbackend.entity.LeagueEntity;
-import com.spp.chekh.pmbackend.entity.PlayerEntity;
-import com.spp.chekh.pmbackend.entity.TeamEntity;
-import com.spp.chekh.pmbackend.service.interfaces.CoachService;
-import com.spp.chekh.pmbackend.service.interfaces.LeagueService;
-import com.spp.chekh.pmbackend.service.interfaces.PlayerService;
-import com.spp.chekh.pmbackend.service.interfaces.TeamService;
+import com.spp.chekh.pmbackend.entity.*;
+import com.spp.chekh.pmbackend.factory.EntityFactory;
+import com.spp.chekh.pmbackend.service.interfaces.*;
+import com.spp.chekh.pmbackend.service.interfaces.document.DocumentDownloadService;
+import com.spp.chekh.pmbackend.service.interfaces.document.DocumentGeneratorService;
 import com.spp.chekh.pmfrontend.exception.ResourceNotFoundException;
+import com.spp.chekh.pmfrontend.view.model.custom.DocumentStatusViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
+import java.io.*;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class DocumentController {
-
-    @Autowired
-    private TeamRosterExcelView teamRosterExcelView;
-
-    @Autowired
-    private LeagueReportExcelView leagueReportExcelView;
-
-    @Autowired
-    private CountryReportExcelView countryReportExcelView;
-
-    @Autowired
-    private PlayerReportWordDocView playerReportWordDocView;
-
-    @Autowired
-    private CoachReportPDFView coachReportPDFView;
 
     @Autowired
     private PlayerService playerService;
@@ -53,8 +34,22 @@ public class DocumentController {
     @Autowired
     private CoachService coachService;
 
+    @Autowired
+    private DocumentGeneratorService documentGenerator;
+
+    @Autowired
+    private EntityFactory entityFactory;
+
+    @Autowired
+    private FileService fileService;
+
+    @Autowired
+    private DocumentDownloadService documentDownloadService;
+
     @RequestMapping(value = "/document/team/{id}/report", method = RequestMethod.GET)
-    public ModelAndView generateTeamRosterExcelDocument(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @ResponseBody
+    public DocumentStatusViewModel generateTeamRosterExcelDocument(@PathVariable int id, HttpServletRequest request){
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
 
         List<PlayerEntity> playerEntities = playerService.findByIdTeam(id);
         TeamEntity teamEntity = teamService.findById(id);
@@ -63,15 +58,24 @@ public class DocumentController {
             throw new ResourceNotFoundException();
         }
 
-        Map<String, Object> teamRosterData = new HashMap<String, Object>();
-        teamRosterData.put("team",teamEntity);
-        teamRosterData.put("roster", playerEntities);
+        String uploadsDir = "/uploads/";
+        String realPathToUploads =  request.getServletContext().getRealPath(uploadsDir);
 
-        return new ModelAndView(teamRosterExcelView,"teamRosterData", teamRosterData);
+        FileEntity fileEntity = entityFactory.getFileEntity("Creating", "", "", "xls");
+        fileService.save(fileEntity);
+        int fileId = fileService.findFirstByOrderByIdDesc().getId();
+
+        documentGenerator.generateTeamExcelReport(teamEntity, playerEntities, fileId, realPathToUploads);
+
+        documentStatusViewModel.setFileId(String.valueOf(fileId));
+        documentStatusViewModel.setStatus("Creating");
+        return documentStatusViewModel;
     }
 
     @RequestMapping(value = "/document/league/{id}/report", method = RequestMethod.GET)
-    public ModelAndView generateLeagueExcelDocument(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @ResponseBody
+    public DocumentStatusViewModel generateLeagueExcelDocument(@PathVariable int id, HttpServletRequest request){
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
 
         List<PlayerEntity> playerEntities = playerService.findAllByLeagueId(id);
         List<TeamEntity> teamEntities = teamService.findByIdLeague(id);
@@ -80,15 +84,24 @@ public class DocumentController {
             throw new ResourceNotFoundException();
         }
 
-        Map<String, Object> leagueReportData = new HashMap<String, Object>();
-        leagueReportData.put("teams", teamEntities);
-        leagueReportData.put("players", playerEntities);
+        String uploadsDir = "/uploads/";
+        String realPathToUploads =  request.getServletContext().getRealPath(uploadsDir);
 
-        return new ModelAndView(leagueReportExcelView,"leagueReportData", leagueReportData);
+        FileEntity fileEntity = entityFactory.getFileEntity("Creating", "", "", "xls");
+        fileService.save(fileEntity);
+        int fileId = fileService.findFirstByOrderByIdDesc().getId();
+
+        documentGenerator.generateLeagueExcelReport(teamEntities, playerEntities, fileId, realPathToUploads);
+
+        documentStatusViewModel.setFileId(String.valueOf(fileId));
+        documentStatusViewModel.setStatus("Creating");
+        return documentStatusViewModel;
     }
 
     @RequestMapping(value = "/document/country/{id}/report", method = RequestMethod.GET)
-    public ModelAndView generateCountryExcelDocument(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @ResponseBody
+    public DocumentStatusViewModel generateCountryExcelDocument(@PathVariable int id, HttpServletRequest request){
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
 
         List<TeamEntity> teamEntities = teamService.findAllByCountryId(id);
         List<LeagueEntity> leagueEntities = leagueService.findByIdCountry(id);
@@ -97,39 +110,102 @@ public class DocumentController {
             throw new ResourceNotFoundException();
         }
 
-        Map<String, Object> countryReportData = new HashMap<String, Object>();
-        countryReportData.put("teams", teamEntities);
-        countryReportData.put("leagues", leagueEntities);
+        String uploadsDir = "/uploads/";
+        String realPathToUploads =  request.getServletContext().getRealPath(uploadsDir);
 
-        return new ModelAndView(countryReportExcelView,"countryReportData", countryReportData);
+        FileEntity fileEntity = entityFactory.getFileEntity("Creating", "", "", "xls");
+        fileService.save(fileEntity);
+        int fileId = fileService.findFirstByOrderByIdDesc().getId();
+
+        documentGenerator.generateCountryExcelReport(leagueEntities, teamEntities, fileId, realPathToUploads);
+
+        documentStatusViewModel.setFileId(String.valueOf(fileId));
+        documentStatusViewModel.setStatus("Creating");
+        return documentStatusViewModel;
     }
 
     @RequestMapping(value = "/document/player/{id}/report", method = RequestMethod.GET)
-    public ModelAndView generatePlayerWordDocument(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+    @ResponseBody
+    public DocumentStatusViewModel generatePlayerWordDocument(@PathVariable int id, HttpServletRequest request){
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
         PlayerEntity playerEntity = playerService.findById(id);
 
         if(playerEntity == null){
             throw new ResourceNotFoundException();
         }
 
-        Map<String, Object> playerReportData = new HashMap<String, Object>();
-        playerReportData.put("player", playerEntity);
+        String uploadsDir = "/uploads/";
+        String realPathToUploads =  request.getServletContext().getRealPath(uploadsDir);
 
-        return new ModelAndView(playerReportWordDocView,"playerReportData", playerReportData);
+        FileEntity fileEntity = entityFactory.getFileEntity("Creating", "", "", "docx");
+        fileService.save(fileEntity);
+        int fileId = fileService.findFirstByOrderByIdDesc().getId();
+
+        documentGenerator.generatePlayerWordReport(playerEntity, fileId, realPathToUploads);
+
+        documentStatusViewModel.setFileId(String.valueOf(fileId));
+        documentStatusViewModel.setStatus("Creating");
+        return documentStatusViewModel;
     }
 
     @RequestMapping(value = "/document/coach/{id}/report", method = RequestMethod.GET)
-    public ModelAndView generatePdfFile(@PathVariable int id, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    @ResponseBody
+    public DocumentStatusViewModel generatePdfFile(@PathVariable int id, HttpServletRequest request) throws Exception {
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
         CoachEntity coachEntity = coachService.findById(id);
 
         if(coachEntity == null){
             throw new ResourceNotFoundException();
         }
 
-        Map<String, Object> coachReportData = new HashMap<String, Object>();
-        coachReportData.put("coach", coachEntity);
+        String uploadsDir = "/uploads/";
+        String realPathToUploads =  request.getServletContext().getRealPath(uploadsDir);
 
-        return new ModelAndView(coachReportPDFView,"coachReportData", coachReportData);
+        FileEntity fileEntity = entityFactory.getFileEntity("Creating", "", "", "pdf");
+        fileService.save(fileEntity);
+        int fileId = fileService.findFirstByOrderByIdDesc().getId();
+
+        documentGenerator.generateCoachPDFReport(coachEntity, fileId, realPathToUploads);
+
+        documentStatusViewModel.setFileId(String.valueOf(fileId));
+        documentStatusViewModel.setStatus("Creating");
+        return documentStatusViewModel;
+    }
+
+    @RequestMapping(value = "/document/{id}/check", method = RequestMethod.GET)
+    @ResponseBody
+    public DocumentStatusViewModel checkDocument(@PathVariable int id) throws FileNotFoundException {
+        DocumentStatusViewModel documentStatusViewModel = new DocumentStatusViewModel();
+        FileEntity fileEntity = fileService.findById(id);
+        if(fileEntity == null){
+            throw new FileNotFoundException();
+        }
+        documentStatusViewModel.setStatus(fileEntity.getStatus());
+        documentStatusViewModel.setFileId(String.valueOf(id));
+        return documentStatusViewModel;
+    }
+
+    @RequestMapping(value = "/document/{id}/download", method = RequestMethod.GET)
+    public void downloadFile(@PathVariable int id, HttpServletRequest request, HttpServletResponse httpServletResponse) throws Exception {
+
+        FileEntity fileEntity = fileService.findById(id);
+        if(fileEntity == null){
+            throw new FileNotFoundException();
+        }
+
+        switch (fileEntity.getType()){
+            case "xls": {
+                documentDownloadService.downloadExcelDocument(fileEntity, httpServletResponse);
+                break;
+            }
+            case "docx": {
+                documentDownloadService.downloadWordDocument(fileEntity, httpServletResponse);
+                break;
+            }
+            case "pdf": {
+                documentDownloadService.downloadPdfDocument(fileEntity, httpServletResponse);
+                break;
+            }
+        }
     }
 }
