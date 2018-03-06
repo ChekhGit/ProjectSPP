f08f3cc0d220b17abd4a5f291d72ab712c607562
ChekhDmitry <dimacheh97@mail.ru>
2018-03-06 20:31:16 +0300

    Document status view model has been added.

---
+++ frontend/src/main/java/com/spp/chekh/pmfrontend/view/model/custom/DocumentStatusViewModel.java
@@ -0,0 +1,23 @@
+package com.spp.chekh.pmfrontend.view.model.custom;
+
+public class DocumentStatusViewModel {
+
+    String fileId;
+    String status;
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
