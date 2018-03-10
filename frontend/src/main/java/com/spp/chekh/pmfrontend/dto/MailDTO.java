b37acb256223bc3cb84585875ea5044e21235b48
ChekhDmitry <dimacheh97@mail.ru>
2018-03-10 18:48:47 +0300

    Mail data transfer object has been created.

---
+++ frontend/src/main/java/com/spp/chekh/pmfrontend/dto/MailDTO.java
@@ -0,0 +1,21 @@
+package com.spp.chekh.pmfrontend.dto;
+
+public class MailDTO {
+
+    private String email;
+
+    public MailDTO() {
+    }
+
+    public MailDTO(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
